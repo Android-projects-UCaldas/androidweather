96517bbe69f13f84c991973c50b86bbdf8b66cfe
lucas <lucas.1701716343@ucaldas.edu.co>
2022-03-25 00:07:15 -0500

    first commit

---
+++ app/src/main/java/com/example/weatheapp/services/model/Root.java
@@ -0,0 +1,9 @@
+package com.example.weatheapp.services.model;
+
+public class Root {
+    private Main main = null;
+
+    public Main getMain() { return main; }
+
+    public void setMain(Main main) { this.main = main; }
+}
